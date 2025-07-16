2c2189438d5fb5a158b86a5c4cadb9e753faec51
ModhuSudhan-oos <toolgenixs@gmail.com>
2025-07-16 21:31:52 +0600

    Create firebase-cnfig.js

---
+++ firebase-cnfig.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyAFIonE-mcnhxFPDSLFiiIs1-tURXqgGYE",
+  authDomain: "modhusudhan-654e7.firebaseapp.com",
+  projectId: "modhusudhan-654e7",
+  storageBucket: "modhusudhan-654e7.appspot.com",
+  messagingSenderId: "221731458319",
+  appId: "1:221731458319:web:127f4d6f18abf1451e027a",
+  measurementId: "G-S8WE56VM8X"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
